096f68f19125f941de1274db761246c8b04504ad
Melis Filiz Esenkaya <127357851+Filiz5@users.noreply.github.com>
2025-02-16 10:25:13 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,6 +34,30 @@ resource "aws_iam_instance_profile" "ec2-profile" {
   role = aws_iam_role.aws_access.name
 }
 
+resource "aws_security_group" "jenkins-sec-gr" {
+  name = "${var.jenkins_sg}-${var.user}"
+  tags = {
+    Name = var.jenkins_sg
+  }
+
+  dynamic "ingress" {
+    for_each = [22, 6443, 80, 3000, 5432, 443, 8081, 30001, 8080, 8090]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    protocol    = -1
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "k3s-server" {
   ami                    = "ami-04b70fa74e45c3917"  # Ubuntu 22.04 LTS (us-east-1)
   instance_type          = var.instancetype
