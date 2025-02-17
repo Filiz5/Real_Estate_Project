e968b194bc5252feae5b542df723cc64856f3afe
Melis Filiz Esenkaya <127357851+Filiz5@users.noreply.github.com>
2025-02-17 16:45:23 +0100

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,9 @@
 output "public_ip" {
     value = aws_instance.k3s-server.public_ip
+    description = "EC2 Public IP adresi"
+}
+
+output "instance_id" {
+    value = aws_instance.k3s-server.id
+    description = "EC2 Instance ID"
 }
